ca18b4218ba26fe840019a3f674466f197070c79
petermsamba <pmsamba@byupathway.edu>
2026-05-20 02:31:56 +0200

    creating and connecting app to the database. Dispaying database results in ejs. Connecting to database from Node.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import 'dotenv/config';
 import express from 'express';
 import { fileURLToPath } from 'url';
 import path from 'path';
+import { testConnection } from './src/models/db.js';
+import { getAllOrganizations } from './src/models/organizations.js';
 
 // Define the application environment
 const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
@@ -30,8 +32,11 @@ app.get('/', (req, res) => {
 });
 
 // Organizations Route
-app.get('/organizations', (req, res) => {
-    res.render('organizations', { title: 'Organizations' });
+app.get('/organizations', async (req, res) => {
+    const organizations = await getAllOrganizations();
+    const title = 'Our Partner Organizations';
+
+    res.render('organizations', { title, organizations });
 });
 
 // Projects Route
@@ -47,7 +52,12 @@ app.get('/categories', (req, res) => {
 /* ==========================================
    SERVER START
    ========================================== */
-app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`);
-    console.log(`Environment: ${NODE_ENV}`);
+app.listen(PORT, async () => {
+    try {
+        await testConnection();
+        console.log(`Server is running at http://127.0.0.1:${PORT}`);
+        console.log(`Environment: ${NODE_ENV}`);
+    } catch (error) {
+        console.error('Error connecting to the database:', error);
+    }
 });
